331c960bdf0b6dc8e51bf85df294ec8fc9b2344b
Maira Sayuri Iida <maira.iida@UNIVESP.LOCAL>
2019-05-14 11:25:13 -0300

    Drag and Drop 2D Unity

---
+++ Unity Drag and Drop 2D/Assets/_Script/DragAndDropUI.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragAndDropUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private Camera cameraPrincipal;
+    [SerializeField] private Canvas canvas;
+
+    private bool estaDentro;
+    private bool estaArrastando;
+
+    private void Update()
+    {
+        if (estaDentro && Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            estaArrastando = true;
+        }
+
+        if (estaArrastando)
+        {
+            transform.position = cameraPrincipal.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, canvas.planeDistance));
+        }
+
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            estaArrastando = false;
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        estaDentro = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        estaDentro = false;
+    }
+}
