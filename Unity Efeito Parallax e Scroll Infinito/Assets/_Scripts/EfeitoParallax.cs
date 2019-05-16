94872e3b9143f527414acd507d5cc4388defd89d
Maira Sayuri Iida <maira.iida@UNIVESP.LOCAL>
2019-05-16 13:59:50 -0300

    Tutorial Feito

---
+++ Unity Efeito Parallax e Scroll Infinito/Assets/_Scripts/EfeitoParallax.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class EfeitoParallax : MonoBehaviour
+{
+    [SerializeField] private Image fundo;
+    [SerializeField] private float velocidade;
+
+    private void Update()
+    {
+        MoveFundo();
+    }
+
+    public void MoveFundo()
+    {
+        transform.position = new Vector3(transform.position.x - velocidade * Time.deltaTime * Input.GetAxisRaw("Horizontal"), 0, 0);
+
+        if(transform.localPosition.x >= fundo.preferredWidth)
+        {
+            transform.localPosition = new Vector3(transform.localPosition.x - fundo.preferredWidth * 2, 0, 0);            
+        } else if(transform.localPosition.x <= -fundo.preferredWidth)
+        {
+            transform.localPosition = new Vector3(transform.localPosition.x + fundo.preferredWidth * 2, 0, 0);
+        }
+    }
+}
