8892b72c2732a3a54a42aa73bfb212fe1be1959e
Maira Sayuri Iida <maira.iida@UNIVESP.LOCAL>
2019-06-06 17:21:02 -0300

    Tutorial Feito

---
+++ Unity Transição de Tela/Assets/_Scripts/TransicaoCena.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransicaoCena : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    private int cenaIndice;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            IniciaTransicao(1);
+        }
+    }
+
+    //Esse método é chamado em qualquer evento que troque a cena do jogo
+    public void IniciaTransicao(int _cenaIndice)
+    {
+        cenaIndice = _cenaIndice;
+        animator.SetTrigger("Inicia");
+    }
+
+    //Esse método é chamado no final da animação
+    public void TrocaCena()
+    {
+        SceneManager.LoadScene(cenaIndice);
+    }
+}
