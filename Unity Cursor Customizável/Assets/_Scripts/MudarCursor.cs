b8b79d3450a053d98ca5011db0087bc28f9dcbd6
Maira Sayuri Iida <maira.iida@DESKTOP-ESTEVAN.univesp.local>
2019-08-07 19:53:40 -0300

    Tutorial Feito

---
+++ Unity Cursor Customizável/Assets/_Scripts/MudarCursor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MudarCursor : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private Texture2D hoverCursor;
+    [SerializeField] private Vector2 customHotspot;
+
+    private Vector2 defaultHotspot = new Vector2(14, 8);
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Cursor.SetCursor(hoverCursor, customHotspot, CursorMode.Auto);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Cursor.SetCursor(null, defaultHotspot, CursorMode.Auto);        
+    }
+}
