98a103e572376a64ce4835e58078a5bbb6ca2d4b
Maira Sayuri Iida <maira.iida@DESKTOP-ESTEVAN.univesp.local>
2020-01-24 13:31:58 -0300

    Tutorial Feito

---
+++ Unity Scriptable Objects/Assets/_Scripts/Bestiario.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Bestiario : MonoBehaviour
+{
+    [SerializeField] private MonstroObject[] monstros;
+    private int _indice = 0;
+
+    [SerializeField] private Image _monstroImage;
+
+    [SerializeField] private TextMeshProUGUI _nome;
+
+    [SerializeField] private TextMeshProUGUI _numero;
+    [SerializeField] private TextMeshProUGUI _lv;
+    [SerializeField] private TextMeshProUGUI _hp;
+    [SerializeField] private TextMeshProUGUI _xp;
+    [SerializeField] private TextMeshProUGUI _item;
+
+    [SerializeField] private TextMeshProUGUI _descricao;
+
+    private void Start()
+    {
+        DefinirDados(monstros[_indice].sprite, monstros[_indice].Nome, monstros[_indice].Id,
+                     monstros[_indice].Nivel, monstros[_indice].Hp, monstros[_indice].Xp,
+                     monstros[_indice].Item, monstros[_indice].Descricao);
+    }
+
+    private void DefinirDados(Sprite sprite, string nome, int id, int lv, int hp, int xp, string item, string descricao)
+    {
+        _monstroImage.sprite = sprite;
+        _nome.text = nome;
+
+        _numero.text = "Nº" + id.ToString();
+        _lv.text = "LV: " + lv.ToString();
+        _hp.text = "HP: " + hp.ToString();
+        _xp.text = "XP: " + xp.ToString();
+        _item.text = item;
+
+        _descricao.text = descricao;
+    }
+
+    public void MudarPagina(int sentido)
+    {
+        _indice += sentido;
+
+        if(_indice >= monstros.Length)
+        {
+            _indice = 0;
+        }else if(_indice < 0)
+        {
+            _indice = monstros.Length - 1;
+        }
+
+        DefinirDados(monstros[_indice].sprite, monstros[_indice].Nome, monstros[_indice].Id,
+                     monstros[_indice].Nivel, monstros[_indice].Hp, monstros[_indice].Xp,
+                     monstros[_indice].Item, monstros[_indice].Descricao);
+    }
+}
---
+++ Unity Scriptable Objects/Assets/_Scripts/MonstroObject.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Novo Monstro", menuName = "Inimigo/Monstro")]
+public class MonstroObject : ScriptableObject
+{
+    public Sprite sprite;
+    public string Nome;
+    public int Id;
+    public int Nivel;
+    public int Hp;
+    public int Xp;
+    public string Item;
+
+    [TextArea]
+    public string Descricao;
+}
