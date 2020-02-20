e8556365e9ca4a614775f26504a0f91219284187
Maira Sayuri Iida <maira.iida@DESKTOP-ESTEVAN.univesp.local>
2020-02-20 16:06:32 -0300

    Feito

---
+++ Unity Object Pooling/Assets/_Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Transform _balaSpawnPoint;
+
+    [SerializeField] private float _tempoDeEspera;
+    private float _tempo;
+
+    [SerializeField] private GameObject _bala;
+
+    private void Update()
+    {
+        _tempo -= Time.deltaTime;
+        if (_tempo <= 0)
+        {
+            Instantiate(_bala, _balaSpawnPoint.position, Quaternion.identity);
+
+            _tempo = _tempoDeEspera;
+        }
+    }
+}
