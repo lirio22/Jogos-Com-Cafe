1f712a340b90528ab9846fd02fba01931d0589e4
Maira Sayuri Iida <maira.iida@DESKTOP-ESTEVAN.univesp.local>
2020-03-06 18:55:12 -0300

    Feito

---
+++ Unity Calendario Pt1 Final/Assets/_Scripts/Dia.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class Dia : MonoBehaviour
+{
+    private TextMeshProUGUI _diaTexto;
+
+    private void Awake()
+    {
+        _diaTexto = gameObject.GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    public void SetDiaAtivo(bool ativo)
+    {
+        _diaTexto.gameObject.SetActive(ativo);
+    }
+
+    public void AtualizarDiaTexto(string novoDia)
+    {
+        _diaTexto.text = novoDia;
+    }
+}
