067d3845410aaf5eca8bc1fb12fdf8b54c2cc507
lirio22 <maira_sayuri@hotmail.com>
2020-05-22 16:57:10 -0300

    Tutorial Feito

---
+++ Unity Musica Continua DIY/Assets/_Scripts/CameraFollow.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform _player;
+    private void Update()
+    {
+        transform.position = new Vector3(_player.position.x, transform.position.y, transform.position.z);
+    }
+}
